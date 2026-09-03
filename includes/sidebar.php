376b5a93c6fb5949db00936c7e259c461a12eab3
claudenine <jcespejo002@gmail.com>
2026-09-03 20:27:48 +0800

    Remove unwanted file

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -155,13 +155,6 @@ $facultyDepartmentHeadNavGroups = [
         ['slug' => 'teaching-load-approval', 'href' => BASE_URL . '/modules/faculty/views/department-head/teaching-load-approval.php', 'icon' => 'fa-layer-group', 'label' => 'Teaching Load Approval'],
     ],
     'Attendance' => [
-
-
-        ['slug' => 'faculty-member-evaluation', 'href' => BASE_URL . '/modules/faculty/views/department-head/attendance.php', 'icon' => 'fa-user-check', 'label' => 'Attendance'],
-
-        ['slug' => 'attendance-summary', 'href' => BASE_URL . '/modules/faculty/views/department-head/attendance-summary.php', 'icon' => 'fa-user-check', 'label' => 'Attendance'],
-
-
         ['slug' => 'attendance-summary', 'href' => BASE_URL . '/modules/faculty/views/department-head/attendance-summary.php', 'icon' => 'fa-user-check', 'label' => 'Attendance'],
 
     ],
